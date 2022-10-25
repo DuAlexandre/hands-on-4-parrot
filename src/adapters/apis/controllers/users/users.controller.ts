c8df9fc1335d2f2c7eb637db2c6d6ff404fd85f1
DuAlexandre <luis.edu.alex@gmail.com>
2022-10-25 14:11:17 -0300

    Create loggers

--- src/adapters/apis/controllers/users/users.controller.ts
+++ src/adapters/apis/controllers/users/users.controller.ts
@@ -5,39 +5,70 @@ import readUserUsecase from "../../../../domain/usecases/users/read.user.usecase
 import createUserUsecase from "../../../../domain/usecases/users/create.user.usecase";
 import updateUserUsecase from "../../../../domain/usecases/users/update.user.usecase";
 import deleteUserUsecase from "../../../../domain/usecases/users/delete.user.usecase";
+import logger from "../../../../infrastructure/logs/winston.logs";
+import constantsConfig from "../../../../infrastructure/config/constants.config";
 
 const log: debug.IDebugger = debug('app:users-controller');
 
 class UsersController {
     async listUsers(req: express.Request, res: express.Response) {
-        const users = await listUserUsecase.execute();
-        res.status(200).send(users);
+        try {
+            const users = await listUserUsecase.execute();
+            res.status(200).send(users);
+        } catch (error) {
+            logger.error('Erro no listUsers do UsersController:', error);
+            res.status(404).send(constantsConfig.USERS.MESSAGES.ERROR.ERROR404);
+        }
+        
     }
 
     async getUserById(req: express.Request, res: express.Response) {
-        const user = await readUserUsecase.execute({
-            idUser: Number(req.params.idUser)
-        });
-        res.status(200).send(user);
+        try {
+            const user = await readUserUsecase.execute({
+                idUser: Number(req.params.idUser)
+            });
+            res.status(200).send(user);
+        } catch (error) {
+            logger.error('Erro no getUserById do UsersController:', error);
+            res.status(404).send(constantsConfig.USERS.MESSAGES.ERROR.ERROR404);
+        }
+        
     }
 
     async createUser(req: express.Request, res: express.Response) {
-        const user = await createUserUsecase.execute(req.body);
-        log(user);
-        res.status(201).send(user);
+        try {
+            const user = await createUserUsecase.execute(req.body);
+            log(user);
+            res.status(201).send(user);
+        } catch (error) {
+            logger.error('Erro no createUsers do UsersController:', error);
+            res.status(500).send(constantsConfig.USERS.MESSAGES.ERROR.ERROR500);
+        }
+        
     }
 
     async updateUser(req: express.Request, res: express.Response) {
-        //let user = await readUserUsecase.execute({idUser: Number(req.params.idUser)});
-        let user = await updateUserUsecase.execute(req.body);
-        res.status(200).send(user);
+        try {
+            let user = await updateUserUsecase.execute(req.body);
+            res.status(200).send(user);
+        } catch (error) {
+            logger.error('Erro no updateUser do UsersController:', error);
+            res.status(400).send(constantsConfig.USERS.MESSAGES.ERROR.ERROR400);
+        }
+        
     }
 
     async deleteUser(req: express.Request, res: express.Response) {
-        const user = await deleteUserUsecase.execute({
-            idUser: Number(req.params.idUser)
-        });
-        res.status(204).send();
+        try {
+            const user = await deleteUserUsecase.execute({
+                idUser: Number(req.params.idUser)
+            });
+            res.status(204).send();
+        } catch (error) {
+            logger.error('Erro no deleteUser do UsersController:', error);
+            res.status(500).send(constantsConfig.USERS.MESSAGES.ERROR.ERROR500);
+        }
+        
     }
 }
 
